6f94eb6e4dacbf417e32d5fd0b2ed2dfd96f8e00
Katherine Aguilera <katherinam35@gmail.com>
2019-07-19 12:28:07 -0500

    Setu react produccion

--- platzi-badges/webpack.dev.config.js
+++ platzi-badges/webpack.dev.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
- module.exports = {
+module.exports = {
   entry: {
     app: path.resolve(__dirname,'src/index.js'),
   },
